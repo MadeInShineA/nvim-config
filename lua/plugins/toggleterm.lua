7ecf41c2b09cddd3f6d2499b78d0e4a5de25933a
MadeInShineA <olivier.amacker@netplus.ch>
2024-09-28 19:18:39 +0200

    Changed the size of the vertical terminal

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,8 +2,12 @@ return {
   "akinsho/toggleterm.nvim",
   version = "*",
   config = true,
+  on_open = function(term)
+    vim.cmd("startinsert")
+    term:send("clear", true)
+  end,
   keys = {
-    { "<leader>th", "<cmd>ToggleTerm<cr>", desc = "Horizontal Terminal (cwd)" },
-    { "<leader>tv", "<cmd>ToggleTerm direction=vertical<cr>", desc = "Vertivcal Terminal (cwd)" },
+    { "<leader>th", "<cmd>ToggleTerm direction=horizontal<cr>", desc = "Horizontal Terminal (cwd)" },
+    { "<leader>tv", "<cmd>ToggleTerm direction=vertical size=80<cr>", desc = "Vertical terminal (cwd)" },
   },
 }
