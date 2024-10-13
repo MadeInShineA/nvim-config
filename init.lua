af4d20f69d8c9e97aa0e3bd7e5ba526208927ea2
MadeInShineA <olivier.amacker@netplus.ch>
2024-10-13 10:34:15 +0200

    Auto changes of init.lua, lazy-lock.json and lazyvim.json

--- init.lua
+++ init.lua
@@ -1,2 +1,19 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+vim.cmd.colorscheme("catppuccin")
+require("catppuccin").setup({
+  transparent_background = true,
+  term_colors = true,
+  styles = {
+    sidebars = "transparent",
+    floats = "transparent",
+  },
+  integrations = {
+    nvimtree = {
+      transparent_panel = true,
+    },
+    -- add more integrations if needed
+  },
+})
+
+vim.cmd.colorscheme("catppuccin")
