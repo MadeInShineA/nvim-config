dadde8477217dd5310101850dd2ea1a2ed7cdd69
MadeInShineA <olivier.amacker@netplus.ch>
2025-01-06 17:59:26 +0100

    Added keymap for neogen + floating toggleterm window

--- lua/plugins/neogen.lua
+++ lua/plugins/neogen.lua
@@ -1,10 +1,8 @@
 return {
   "danymat/neogen",
   config = true,
-
   -- Uncomment next line if you want to follow only stable versions
   -- version = "*"
-
    keys = {
     { "<Leader>cc", ":Neogen<CR>", desc = "Generate Documentation with Neogen" },
   },
